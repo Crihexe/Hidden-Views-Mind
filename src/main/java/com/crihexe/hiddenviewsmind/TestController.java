839216294dc8fb54e7b20981f187015978face00
Cristian Di Nicola <36051144+Crihexe@users.noreply.github.com>
2024-07-20 15:07:59 +0200

    db dump for testing

--- src/main/java/com/crihexe/hiddenviewsmind/TestController.java
+++ src/main/java/com/crihexe/hiddenviewsmind/TestController.java
@@ -1,5 +1,9 @@
 package com.crihexe.hiddenviewsmind;
 
+import com.crihexe.hiddenviewsmind.db.entities.PostEntity;
+import com.crihexe.hiddenviewsmind.db.mongo.PostingQueueMongo;
+import com.crihexe.hiddenviewsmind.db.repositories.PostRepository;
+import com.crihexe.hiddenviewsmind.db.repositories.PostingQueueRepository;
 import com.crihexe.hiddenviewsmind.dto.Post;
 import com.crihexe.hiddenviewsmind.publisher.ContentPublisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,15 +13,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     private final ContentPublisher cp;
+    private final PostRepository postRepo;
+    private final PostingQueueRepository postingQueueRepo;
 
     @Autowired
-    public TestController(ContentPublisher cp) {
+    public TestController(ContentPublisher cp, PostRepository postRepo, PostingQueueRepository postingQueueRepo) {
         this.cp = cp;
+        this.postRepo = postRepo;
+        this.postingQueueRepo = postingQueueRepo;
     }
 
     @GetMapping("/remote")
@@ -36,6 +46,16 @@ public class TestController {
         String post = "{\"id\":null,\"instagram_id\":null,\"mediaType\":null,\"imageURL\":null,\"videoURL\":null,\"caption\":\"swag @crih.exe @_viola.scarda_\",\"carouselItem\":null,\"children\":[],\"userTags\":[],\"audioName\":null,\"shareToFeed\":null,\"locationID\":null,\"coverURL\":null,\"thumbOffset\":null,\"keywords\":[],\"addedAt\":null,\"postedAt\":null}";
         Post p = new ObjectMapper().readValue(post, Post.class);
         return new ObjectMapper().writeValueAsString(p);
+    }
+
+    @GetMapping("/db/mysql/dump")
+    public List<PostEntity> mysqlDump() {
+        return postRepo.findAll();
+    }
+
+    @GetMapping("/db/mongo/dump")
+    public List<PostingQueueMongo> mongoDump() {
+        return postingQueueRepo.findAll();
     }
 
 }
--- src/main/java/com/crihexe/hiddenviewsmind/db/repositories/PostRepository.java
+++ src/main/java/com/crihexe/hiddenviewsmind/db/repositories/PostRepository.java
@@ -4,4 +4,5 @@ import com.crihexe.hiddenviewsmind.db.entities.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+
 }
